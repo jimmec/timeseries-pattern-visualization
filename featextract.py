cefbc6d2b8784ae6f439aabba9845660bf535f1d
Jimmy Cao <jimmec@gmail.com>
2014-04-05 11:57:54 -0700

    fixed indices in featextract

--- featextract.py
+++ featextract.py
@@ -1,25 +1,26 @@
 #! /bin/python
 '''Time series feature extractor
-Author: JumboSliceKimboShrimp
+Author: JumboSliceKimboShrimp & Riceballicious
 
 Given a segmented time series as a list of (index,value) pairs of 
-length N+1, and a constant L<N+1,
-extract fixed length L+1 windows of the segmented time series and output a 
+length N, and a constant L<N,
+extract fixed length L windows of the segmented time series and output a 
 set of L+1 features for each window. 
 
 Each window is a pair (p_start,p_end).
 Each feature set is a tuple (f0,f1,...,fL)
 
 E.g. input: list of segments [p0,p1,p2,...,pN]
-     output: [((p0,pL),(f0,f1,...,fL)), ((p1,pL+1),(f0,f1,...,fL)),...
-                ,((pN-L,pN),(f0,f1,...,fL))]
+     output: [(f00,f01,...,f0L), (f10,f11,...,f1L),...
+                ,(fk0,fk1,...,fkL))], k=N-L+1
 
 '''
-import itertools
 
 def compute_simple_trend_features(window):
-    '''Given a window of N points, compute N+1 trend features as described
+    '''Given a window of N points as a list, 
+    compute N+1 trend features as described
     in http://www.cs.ucsb.edu/~nanli/publications/stock_pattern.pdf
+    and returns a list of N+1 features 
     '''
     midfeats = [(window[i]-window[i-1])/(window[i-1]-window[i-2]) 
         for i in xrange(2,len(window))]
@@ -27,15 +28,20 @@ def compute_simple_trend_features(window):
         .extend(midfeats)
         .append((window[-1]-window[1])/abs(window[2]-window[1]))
 
-    return tuple(feats)
+    return feats
 
     
 def extract_features(data, length, compute_feature):
     '''Given data as a list of (index,value) pairs,
-    and length<len(data),
+    and 1<length<len(data),
     extracts features for sliding windows of width length
+    using the routine compute_feature
     '''
-    return [((data[i],data[i+n]),compute_feature(data[i:i+n])) 
-        for i in xrange(len(data)-n+1)]
+    if length >= len(data) or length < 1:
+        raise Exception(
+            ('Invalid window length=',length, ', data length=',len(data)))
+
+    return [(compute_feature(data[i:i+length])) 
+        for i in xrange(len(data)-length+1)]
     
 
