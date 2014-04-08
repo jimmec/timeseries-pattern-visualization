d6ffe25f938159ab1bd077e63c8c66c8f997a545
Jimmy Cao <jimmec@gmail.com>
2014-04-08 04:51:49 -0700

    added a relative residual error

--- featuregenerator/segmenter.py
+++ featuregenerator/segmenter.py
@@ -13,16 +13,33 @@ import math
 import llist
 import heapdict as hd
 
+def relative_sqr_residual(segment, data):
+    '''
+    Input is a segment of the form
+    ((index1, value1), (index2, value2))
+    where index2 > index1 corresponds to indices in the data
+
+    Calculates a 'relative' residual
+    '''
+    # change in value per change in index
+    m = (segment[1][1] - segment[0][1]) / (segment[1][0] - segment[0][0])
+    # number of intermediate points
+    n = segment[1][0] - segment[0][0]
+
+    pred = [segment[0][1] + m*i for i in xrange(1,n)]
+    value = [data[segment[0][0] + i] for i in xrange(1,n)]
+    sqr_residual = [((pred[i] - value[i])/value[i])**2 for i in xrange(n-1)]
+
+    return sum(sqr_residual)
+
 def sqr_residual(segment, data):
     '''
     Input is a segment of the form
     ((index1, value1), (index2, value2))
     where index2 > index1 corresponds to indices in the data
     '''
-
     # change in value per change in index
     m = (segment[1][1] - segment[0][1]) / (segment[1][0] - segment[0][0])
-
     # number of intermediate points
     n = segment[1][0] - segment[0][0]
 
@@ -35,7 +52,7 @@ def sqr_residual(segment, data):
 def merge_segs(seg1, seg2):
     return (seg1[0], seg2[1])
 
-def bottom_up(data, k, max_error=float('inf')):
+def bottom_up(data, k, calc_error=sqr_residual, max_error=float('inf')):
     '''
     Merge time series data points to produce trend segments
     using bottom-up method where k is the average segment length.
@@ -49,6 +66,9 @@ def bottom_up(data, k, max_error=float('inf')):
     If max_error > 0, when the maximum error of any 
     segment (in terms of a residual with the original data underlying the 
     segment) has reached the limit.
+
+    calc_error(segment, data) provides the specific implementation to
+    compute the error of a segment.
     
     Returns a subset of the data in the form of [(index, value)].
     '''
@@ -61,7 +81,7 @@ def bottom_up(data, k, max_error=float('inf')):
     segments = [((2*i,data[2*i]), (2*i+1,data[2*i+1])) for i in xrange(n/2)]
 
     # create linked list of pairs of segments
-    # and a heap dictionary of (pair -> sqr_residual) 
+    # and a heap dictionary of (pair -> residual error) 
     pairs = llist.dllist()
     res_heap = hd.heapdict()
 
@@ -71,8 +91,8 @@ def bottom_up(data, k, max_error=float('inf')):
         right = segments[i+1]
         seg_pair = (left, right)
         
-        # get the squared residual
-        res = sqr_residual(merge_segs(left, right), data)
+        # get the residual error
+        res = calc_error(merge_segs(left, right), data)
 
         # add to linked list and heap
         node = pairs.append(seg_pair)
@@ -95,7 +115,7 @@ def bottom_up(data, k, max_error=float('inf')):
             lpair = left.value
             left.value = (lpair[0], new_seg)
             merged = merge_segs(left.value[0], left.value[1])
-            res_heap[left] = sqr_residual(merged, data)
+            res_heap[left] = calc_error(merged, data)
 
         # update first segment of right pair
         right = pair.next
@@ -104,7 +124,7 @@ def bottom_up(data, k, max_error=float('inf')):
             rpair = right.value
             right.value = (new_seg, rpair[1])
             merged = merge_segs(right.value[0], right.value[1])
-            res_heap[right] = sqr_residual(merged, data)
+            res_heap[right] = calc_error(merged, data)
 
         # remove old pair from pairs linked list
         pairs.remove(pair)
